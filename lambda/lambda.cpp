fd2cdd4ac4a9e79635013b94df2d6637bce2b192
Ben Lawrence <ben.lawrence@microfocus.com>
2023-08-22 21:45:24 +0100

    using lambdas to calculate annuities (probably incorrectly)

---
+++ lambda/lambda.cpp
@@ -0,0 +1,19 @@
+// lambda.cpp : example of using a lambda to calculate an annuity
+//
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+int main()
+{
+	std::vector<double> times = { 1, 2, 3, 4, 5 };
+	double PMT = 1000.0;
+	double r = 0.05;
+
+	double total_ann = std::accumulate(times.begin(), times.end(), 0.0, [PMT, r](const double& acc, const double& x) {
+		return acc + (PMT * (1 - std::pow(1 + r, -x)) / r);
+		});
+
+	std::cout << total_ann << std::endl;
+}
